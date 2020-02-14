be87f2b1dbe642a2e4a4364c69a2c7a558732bbb
Carlos Ratia <carlos.ratia@bunkerdb.com>
2020-02-14 16:05:49 -0300

    Fix in the Cratia\Rest\Middleware::RouteInfo()

--- src/Middleware/RouteInfo.php
+++ src/Middleware/RouteInfo.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Cratia\Rest\Middleware;
 
 
+use Cratia\Rest\Dependencies\AppManager;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\App;
 use Slim\Interfaces\RouteInterface;
 use Slim\Routing\Route;
 use Slim\Routing\RouteContext;
@@ -19,6 +22,28 @@ use Slim\Routing\RouteContext;
  */
 class RouteInfo implements MiddlewareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Context constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer(): ContainerInterface
+    {
+        return $this->container;
+    }
+
     /**
      * @param ServerRequestInterface $request
      * @param RequestHandlerInterface $handler
@@ -34,12 +59,16 @@ class RouteInfo implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        /** @var RouteContext $routeContext */
-        $routeContext = RouteContext::fromRequest($request);
-        /** @var Route $route */
-        $route = $routeContext->getRoute();
-        if ($route instanceof RouteInterface) {
-            AppManager::getInstance()->setRoute($route);
+        if ($this->getContainer()->has(AppManager::class)) {
+            /** @var AppManager $appManager */
+            $appManager = $this->getContainer()->has(AppManager::class);
+            /** @var RouteContext $routeContext */
+            $routeContext = RouteContext::fromRequest($request);
+            /** @var Route $route */
+            $route = $routeContext->getRoute();
+            if ($route instanceof RouteInterface) {
+                $appManager->setRoute($route);
+            }
         }
         $response = $handler->handle($request);
         return $response;
